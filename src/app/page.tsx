39893e5e0515ca1de54d808ee83a30626b531dee
cisarda <cisarda@student.uzlabina.cz>
2024-02-29 15:03:34 +0100

    jommjmjmjj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 export default function Home() {
-      <div className="box">
-            <button>On</button>
-            <button>Off</button>
-      </div>
+      return (
+                <div className="flex justify-center items-start h-screen w-screen pt-2">
+                    <div className="flex flex-row justify-evenly bg-slate-200 py-12 px-8">
+                        <div className="flex items-start gap-12">
+                            <div className="">Home</div>
+                            <div className="">Contacts</div>
+                            <div className="">Categories</div>
+                        </div>
+                        <div>
+                            <div className="flex justify-end pl-4">Sign in</div>
+                        </div>
+                    </div>
+                </div>
+      );
 }
