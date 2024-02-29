6b176ce7a941d2b11bd2f8cadb9769ec69db6241
cisarda <cisarda@student.uzlabina.cz>
2024-02-29 15:12:52 +0100

    jommjmjmjj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 export default function Home() {
       return (
-                <div className="flex justify-center items-start h-screen w-screen pt-2">
-                    <div className="flex flex-row justify-evenly bg-slate-200 py-12 px-8">
+          <main className="h-screen mr-[170px] ml-[170px] mt-[92px]">
+          <div className="flex justify-center items-start">
+                    <div className="flex flex-row justify-between bg-neutral-200 py-12 px-8">
                         <div className="flex items-start gap-12">
                             <div className="">Home</div>
                             <div className="">Contacts</div>
                             <div className="">Categories</div>
                         </div>
                         <div>
-                            <div className="flex justify-end pl-4">Sign in</div>
+                            <div className="flex justify-end pl-40">Sign in</div>
                         </div>
                     </div>
                 </div>
+          </main>
       );
 }
